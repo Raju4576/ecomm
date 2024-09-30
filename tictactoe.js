729ce50546261fa2dadf39d568bb15eb08a7fc7e
viveksheladiya698 <viveksheladiya698@gmail.com>
2024-09-30 10:36:08 +0530

    add new file

---
+++ tictactoe.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Tictactoe = () => {
+  return (
+    <View>
+      <Text>Tictactoe</Text>
+    </View>
+  )
+}
+
+export default Tictactoe
+
+const styles = StyleSheet.create({})
